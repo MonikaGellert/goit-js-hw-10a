a3ef4973f35e3241eac970d9b7b774f9d9559b36
MonikaGellert <113904942+MonikaGellert@users.noreply.github.com>
2023-07-17 20:18:06 +0200

    Add files via upload

---
+++ src/cat-api.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import Notiflix from 'notiflix';
+import { loaderTextRef, errorRef } from './js/index.js';
+
+const urlAllBreeds = `https://api.thecatapi.com/v1/breeds`;
+const urlSearchBreed = 'https://api.thecatapi.com/v1/images/search';
+
+const api_key =
+  'live_jWNukllSziHsgvhYm1XBWDhhnJ1f8oaOEEvSDUOkNedfFqrIxiTyDUHs3nCuK14W';
+
+const fetchBreeds = breedsCats =>
+  fetch(urlAllBreeds, {
+    headers: {
+      'x-api-key': api_key,
+    },
+  }).then(response => {
+    if (!response.ok) {
+      Notiflix.Report.failure(
+        'Oops!',
+        'Something went wrong! Try reloading the page!',
+        'Okay'
+      );
+      loaderTextRef.classList.add('visually-hidden');
+      errorRef.classList.remove('visually-hidden');
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+
+const fetchCatByBreed = breedId =>
+  fetch(urlSearchBreed + `?breed_ids=${breedId}`, {
+    headers: {
+      'x-api-key': api_key,
+    },
+  }).then(response => {
+    if (!response.ok) {
+      Notiflix.Report.failure(
+        'Oops!',
+        'Something went wrong! Try reloading the page!',
+        'Okay'
+      );
+      loaderTextRef.classList.add('visually-hidden');
+      errorRef.classList.remove('visually-hidden');
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+
+export { fetchBreeds, fetchCatByBreed };
---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+export { loaderTextRef, errorRef };
+
+const selectBreedCat = document.querySelector('.breed-select');
+const cardRef = document.querySelector('.cat-info');
+const loaderTextRef = document.querySelector('.loader-text');
+const cssloaderRef = document.querySelector('span.loader');
+const errorRef = document.querySelector('.error');
+
+fetchBreeds().then(breeds => {
+  let markupOptions = breeds
+    .map(breed => `<option value=${breed.id}>${breed.name}</option>`)
+    .join('');
+
+  selectBreedCat.insertAdjacentHTML('afterbegin', markupOptions);
+  selectBreedCat.classList.remove('visually-hidden');
+  cssloaderRef.classList.add('visually-hidden');
+  loaderTextRef.classList.add('visually-hidden');
+});
+
+selectBreedCat.addEventListener('change', onChengeBreed);
+
+function onChengeBreed() {
+  let breedId = selectBreedCat.value;
+  errorRef.classList.add('visually-hidden');
+  loaderTextRef.classList.remove('visually-hidden');
+  cssloaderRef.classList.remove('visually-hidden');
+  cardRef.classList.add('visually-hidden');
+  cardRef.innerHTML = '';
+  fetchCatByBreed(breedId).then(foundCat => {
+    if (foundCat[0] === undefined) {
+      Notiflix.Report.failure(
+        'Oops!',
+        'Сat with this breed is not found! Choose another breed!',
+        'Okay'
+      );
+      cssloaderRef.classList.add('visually-hidden');
+      loaderTextRef.classList.add('visually-hidden');
+      errorRef.classList.remove('visually-hidden');
+      return;
+    }
+    const imgUrl = foundCat[0].url;
+    const aboutCat = foundCat[0].breeds[0];
+    const description = aboutCat.description;
+    const name = aboutCat.name;
+    const temperament = aboutCat.temperament;
+    cardRef.innerHTML = ` <img
+        src="${imgUrl}"
+        alt="${name}"
+        width="360"
+      />
+      <div class="description">
+        <h2 class="title">${name}</h2>
+        <p>
+          ${description}
+        </p>
+        <p><b>Temperament:</b> ${temperament}</p>
+      </div>`;
+
+    cardRef.classList.remove('visually-hidden');
+    cssloaderRef.classList.add('visually-hidden');
+    loaderTextRef.classList.add('visually-hidden');
+  });
+}
